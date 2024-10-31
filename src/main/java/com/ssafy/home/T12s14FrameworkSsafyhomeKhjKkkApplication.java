6702c38e563bf50a8ef79ebbc8db69e4e9c0d98a
becooq81 <becooq81@gmail.com>
2024-11-01 08:04:35 +0900

    feat: initial commit

---
+++ src/main/java/com/ssafy/home/T12s14FrameworkSsafyhomeKhjKkkApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T12s14FrameworkSsafyhomeKhjKkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T12s14FrameworkSsafyhomeKhjKkkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/home/T12s14FrameworkSsafyhomeKhjKkkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T12s14FrameworkSsafyhomeKhjKkkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
