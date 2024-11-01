d3a29f772d849866b922e36bbf90053b85308582
becooq81 <becooq81@gmail.com>
2024-11-01 11:52:41 +0900

    feat: set up overriding (early commit due to rebase)

--- src/main/java/com/ssafy/home/location/service/BasicLocationService.java
+++ src/main/java/com/ssafy/home/location/service/BasicLocationService.java
@@ -1,5 +1,20 @@
 package com.ssafy.home.location.service;
 
+import com.ssafy.home.location.payload.response.DongList;
+import com.ssafy.home.location.payload.response.GugunList;
+
 public class BasicLocationService implements LocationService {
+
+	@Override
+	public DongList findDongBySiGugun(String si, String gugun) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GugunList findGugunBySi(String si) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
