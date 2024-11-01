664a6fadaea58a49e18201a379e1b15ffb91eade
becooq81 <becooq81@gmail.com>
2024-11-01 11:38:32 +0900

    feat: set up repository interfaces

---
+++ src/main/java/com/ssafy/home/location/repository/LocationRepository.java
@@ -0,0 +1,5 @@
+package com.ssafy.home.location.repository;
+
+public interface LocationRepository {
+
+}
---
+++ src/main/java/com/ssafy/home/user/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.ssafy.home.user.repository;
+
+public interface UserRepository {
+
+}
