d2984fd42d920034f978feb521e0daf367930e6d
becooq81 <becooq81@gmail.com>
2024-11-01 13:23:53 +0900

    feat: complete location controller

--- src/main/java/com/ssafy/home/location/controller/BasicLocationController.java
+++ src/main/java/com/ssafy/home/location/controller/BasicLocationController.java
@@ -2,8 +2,15 @@ package com.ssafy.home.location.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.home.location.payload.response.DongCode;
+import com.ssafy.home.location.payload.response.DongList;
+import com.ssafy.home.location.payload.response.GugunList;
+import com.ssafy.home.location.service.LocationService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,22 +19,27 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class BasicLocationController implements LocationController {
 
+	private final LocationService service;
+
 	@Override
-	public List<String> getGugunList(String sidoName) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<GugunList> getGugunList(String sidoName) {
+		GugunList gugunList = service.findGugunBySi(sidoName);
+		return new ResponseEntity<>(gugunList, HttpStatus.OK);
 	}
 
 	@Override
-	public List<String> getDongList(String sidoName, String gugunName) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<DongList> getDongList(String sidoName, String gugunName) {
+		DongList dongList = service.findDongBySiGugun(sidoName, gugunName);
+		return new ResponseEntity<>(dongList, HttpStatus.OK);
 	}
 
 	@Override
-	public List<String> getDongCode(String sidoName, String gugunName, String dongName) {
-		// TODO Auto-generated method stub
-		return null;
+	public ResponseEntity<DongCode> getDongCode(String sidoName, String gugunName, String dongName) {
+		DongCode dongcode = service.findDongCode(sidoName, gugunName, dongName);
+		return new ResponseEntity<>(dongcode, HttpStatus.OK);
 	}
+
+	
+	
 
 }
