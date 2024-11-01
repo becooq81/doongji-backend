e89a1bcb0659aa3f6e7f764247e57b874b45c22a
becooq81 <becooq81@gmail.com>
2024-11-01 12:39:42 +0900

    feat(location): implement location service

--- src/main/java/com/ssafy/home/location/service/BasicLocationService.java
+++ src/main/java/com/ssafy/home/location/service/BasicLocationService.java
@@ -1,20 +1,62 @@
 package com.ssafy.home.location.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.home.location.payload.response.DongCode;
 import com.ssafy.home.location.payload.response.DongList;
 import com.ssafy.home.location.payload.response.GugunList;
+import com.ssafy.home.location.repository.LocationRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class BasicLocationService implements LocationService {
 
+	private final LocationRepository repo;
+	
+	/**
+	 * 시, 군구에 부합하는 모든 동을 찾는다
+	 */
 	@Override
 	public DongList findDongBySiGugun(String si, String gugun) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> dongNames = repo.selectDong(si, gugun);
+		DongList dongList = DongList.builder()
+				.si(si)
+				.gugun(gugun)
+				.dongList(dongNames)
+				.build();
+		return dongList;
 	}
 
+	/**
+	 * 시에 부합하는 모든 구군을 찾는다
+	 */
 	@Override
 	public GugunList findGugunBySi(String si) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> gugunNames = repo.selectGugun(si);
+		GugunList gugunList = GugunList.builder()
+				.si(si)
+				.gugunList(gugunNames)
+				.build();
+		return gugunList;
+	}
+
+	/**
+	 * 시, 구군, 동에 부합하는 동코드를 찾는다
+	 */
+	@Override
+	public DongCode findDongCode(String si, String gugun, String dong) {
+		String dongcodeVal = repo.selectDongCode(si, gugun, dong);
+		DongCode dongcode = DongCode.builder()
+				.si(si)
+				.gugun(gugun)
+				.dong(dong)
+				.dongcode(dongcodeVal)
+				.build();
+		return dongcode;
 	}
 
 }
--- src/main/java/com/ssafy/home/location/service/LocationService.java
+++ src/main/java/com/ssafy/home/location/service/LocationService.java
@@ -1,9 +1,11 @@
 package com.ssafy.home.location.service;
 
+import com.ssafy.home.location.payload.response.DongCode;
 import com.ssafy.home.location.payload.response.DongList;
 import com.ssafy.home.location.payload.response.GugunList;
 
 public interface LocationService {
 	DongList findDongBySiGugun(String si, String gugun);
 	GugunList findGugunBySi(String si);
+	DongCode findDongCode(String si, String gugun, String dong);
 }
