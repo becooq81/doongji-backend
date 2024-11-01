b693b2ca78044898fbf8e0c3cd01f0767df396fe
becooq81 <becooq81@gmail.com>
2024-11-01 11:47:01 +0900

    feat: declare methods for location service interface

--- src/main/java/com/ssafy/home/location/service/LocationService.java
+++ src/main/java/com/ssafy/home/location/service/LocationService.java
@@ -1,5 +1,9 @@
 package com.ssafy.home.location.service;
 
-public interface LocationService {
+import com.ssafy.home.location.payload.response.DongList;
+import com.ssafy.home.location.payload.response.GugunList;
 
+public interface LocationService {
+	DongList findDongBySiGugun(String si, String gugun);
+	GugunList findGugunBySi(String si);
 }
