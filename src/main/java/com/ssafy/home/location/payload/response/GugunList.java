0057f9dd8e1aad7da7458391e1a22d24942b0c52
becooq81 <becooq81@gmail.com>
2024-11-01 11:46:50 +0900

    feat: add constructor annotations for dtos

--- src/main/java/com/ssafy/home/location/payload/response/GugunList.java
+++ src/main/java/com/ssafy/home/location/payload/response/GugunList.java
@@ -2,9 +2,13 @@ package com.ssafy.home.location.payload.response;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GugunList {
 
 	private String si;
