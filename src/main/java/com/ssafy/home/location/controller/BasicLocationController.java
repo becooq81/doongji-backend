753ce141a27d9eee82f4cd130752fb27a5ae3db8
becooq81 <becooq81@gmail.com>
2024-11-01 12:39:13 +0900

    refactor(location): rename controller interface

--- src/main/java/com/ssafy/home/location/controller/BasicLocationController.java
+++ src/main/java/com/ssafy/home/location/controller/BasicLocationController.java
@@ -1,5 +1,33 @@
 package com.ssafy.home.location.controller;
 
-public class BasicLocationController implements BasicLocation {
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/location")
+@RestController
+public class BasicLocationController implements LocationController {
+
+	@Override
+	public List<String> getGugunList(String sidoName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getDongList(String sidoName, String gugunName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> getDongCode(String sidoName, String gugunName, String dongName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
