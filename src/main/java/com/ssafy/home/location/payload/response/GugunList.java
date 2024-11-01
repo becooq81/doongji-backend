77f6a2c720fddaeb306ffe2e4c26808d54d219b6
becooq81 <becooq81@gmail.com>
2024-11-01 12:39:25 +0900

    feat: add builder to payloads

--- src/main/java/com/ssafy/home/location/payload/response/GugunList.java
+++ src/main/java/com/ssafy/home/location/payload/response/GugunList.java
@@ -3,12 +3,14 @@ package com.ssafy.home.location.payload.response;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class GugunList {
 
 	private String si;
