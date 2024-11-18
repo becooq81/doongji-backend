3316c695ee0a8a143d3123e6082ce4ded0b27159
becooq81 <becooq81@gmail.com>
2024-11-19 01:13:17 +0900

    feat: add search history payload to contain what user searched previously

--- src/main/java/com/find/doongji/search/payload/SearchHistory.java
+++ src/main/java/com/find/doongji/search/payload/SearchHistory.java
@@ -1,4 +1,22 @@
 package com.find.doongji.search.payload;
 
-public class Search {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchHistory {
+
+    private String username;
+    private String query;
+
+    @Override
+    public String toString() {
+        return "SearchHistory{" +
+                "username='" + username + '\'' +
+                ", query='" + query + '\'' +
+                '}';
+    }
 }
