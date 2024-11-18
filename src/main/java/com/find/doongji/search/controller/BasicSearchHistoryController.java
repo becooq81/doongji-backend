a13f5c3a0c5609d5eadafac4821441e74d999f66
becooq81 <becooq81@gmail.com>
2024-11-19 01:33:12 +0900

    feat: add controller & impl for search history

--- src/main/java/com/find/doongji/search/controller/BasicSearchHistoryController.java
+++ src/main/java/com/find/doongji/search/controller/BasicSearchHistoryController.java
@@ -1,4 +1,41 @@
 package com.find.doongji.search.controller;
 
-public class BasicSearchHistoryController {
+import com.find.doongji.search.payload.request.SearchHistoryRequest;
+import com.find.doongji.search.payload.response.SearchHistoryResponse;
+import com.find.doongji.search.service.SearchHistoryService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/search")
+@RequiredArgsConstructor
+public class BasicSearchHistoryController implements SearchHistoryController{
+
+    private final SearchHistoryService service;
+
+    @Override
+    @PostMapping("/{username}")
+    public ResponseEntity<?> addSearchHistory(@PathVariable String username,
+                                              @RequestBody @Valid SearchHistoryRequest searchHistoryRequest) {
+        service.addSearchHistory(searchHistoryRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @GetMapping("/{username}")
+    public ResponseEntity<?> getSearchHistoryByUsername(@PathVariable String username) {
+        List<SearchHistoryResponse> searchHistories = service.getSearchHistory(username);
+        return new ResponseEntity<>(searchHistories, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteSearchHistoryById(Long id) {
+        service.removeSearchHistory(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/find/doongji/search/controller/SearchHistoryController.java
+++ src/main/java/com/find/doongji/search/controller/SearchHistoryController.java
@@ -1,4 +1,30 @@
 package com.find.doongji.search.controller;
 
-public class SearchHistoryController {
+import com.find.doongji.search.payload.request.SearchHistoryRequest;
+import org.springframework.http.ResponseEntity;
+
+public interface SearchHistoryController {
+
+    /**
+     * Adds a new search history record for the given user.
+     * @param username The username of the user who made the search.
+     * @param searchHistoryRequest
+     * @return ResponseEntity with status code 201 (Created) upon successful creation.
+     */
+    ResponseEntity<?> addSearchHistory(String username, SearchHistoryRequest searchHistoryRequest);
+
+    /**
+     * Retrieves all search history records for a specific user, ordered by the most recent search.
+     * @param username The username whose search history is to be fetched.
+     * @return ResponseEntity with status code 200 (OK) and a list of search history records.
+     */
+    ResponseEntity<?> getSearchHistoryByUsername(String username);
+
+    /**
+     * Deletes a specific search history record by its unique ID.
+     *
+     * @param id The ID of the search history record to be deleted.
+     * @return ResponseEntity with status code 200 (OK) upon successful deletion.
+     */
+    ResponseEntity<Void> deleteSearchHistoryById(Long id);
 }
