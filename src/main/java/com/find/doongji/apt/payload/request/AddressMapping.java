63d0a3267a32a8bcd48dd6bd04eb0a38cb2f0572
becooq81 <becooq81@gmail.com>
2024-11-20 02:14:37 +0900

    feat: add payload request to be inserted into address mapping table

--- src/main/java/com/find/doongji/apt/payload/request/AddressMapping.java
+++ src/main/java/com/find/doongji/apt/payload/request/AddressMapping.java
@@ -1,4 +1,15 @@
 package com.find.doongji.apt.payload.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddressMapping {
+    private String oldAddress;
+    private String umdNm;
+    private String jibun;
+    private String aptSeq;
 }
