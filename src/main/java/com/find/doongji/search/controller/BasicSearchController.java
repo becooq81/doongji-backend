0a8ecc98a2b52ef5d484547aa8e4ae916dadcf4a
becooq81 <becooq81@gmail.com>
2024-11-20 05:18:33 +0900

    feat: implement search

--- src/main/java/com/find/doongji/search/controller/BasicSearchController.java
+++ src/main/java/com/find/doongji/search/controller/BasicSearchController.java
@@ -1,4 +1,30 @@
 package com.find.doongji.search.controller;
 
+import com.find.doongji.search.payload.request.SearchRequest;
+import com.find.doongji.search.service.SearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/search")
+@RequiredArgsConstructor
 public class BasicSearchController {
+
+    private final SearchService searchService;
+
+    @PostMapping
+    public ResponseEntity<?> search(@RequestBody SearchRequest searchRequest) {
+        try {
+            return ResponseEntity.ok(searchService.search(searchRequest));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body("An error occurred while processing the search request.");
+        }
+
+    }
+
 }
--- src/main/java/com/find/doongji/search/service/SearchService.java
+++ src/main/java/com/find/doongji/search/service/SearchService.java
@@ -1,4 +1,12 @@
 package com.find.doongji.search.service;
 
+import com.find.doongji.apt.payload.response.SearchResult;
+import com.find.doongji.search.payload.request.SearchRequest;
+
+import java.util.List;
+
 public interface SearchService {
+
+    List<SearchResult> search(SearchRequest searchRequest) throws Exception;
+
 }
