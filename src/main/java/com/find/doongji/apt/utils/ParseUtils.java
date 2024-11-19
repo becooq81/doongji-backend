996423aebe07530e7ff8baf2005c2679a9fb1225
becooq81 <becooq81@gmail.com>
2024-11-19 10:00:33 +0900

    refactor: modularize method to parse json & xml

--- src/main/java/com/find/doongji/apt/utils/ParseUtils.java
+++ src/main/java/com/find/doongji/apt/utils/ParseUtils.java
@@ -4,6 +4,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,7 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class JsonUtils {
+public class ParseUtils {
 
     public static List<Map<String, String>> parseXML(String responseBody, String... keys) throws Exception{
         List<Map<String, String>> result = new ArrayList<>();
@@ -36,6 +38,33 @@ public class JsonUtils {
                     result.get(i).put(key, item.getElementsByTagName(key).item(0).getTextContent());
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public static List<Map<String, String>> parseJson(String responseBody, String... keys) throws Exception {
+        List<Map<String, String>> result = new ArrayList<>();
+
+        try {
+            JSONObject jsonResponse = new JSONObject(responseBody);
+
+            // Extract resultdata array from the JSON
+            JSONArray resultDataArray = jsonResponse.getJSONObject("result").getJSONArray("resultdata");
+
+            for (int i = 0; i < resultDataArray.length(); i++) {
+                JSONObject item = resultDataArray.getJSONObject(i);
+                Map<String, String> itemMap = new HashMap<>();
+
+                for (String key : keys) {
+                    itemMap.put(key, item.getString(key));
+                }
+
+                result.add(itemMap);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
