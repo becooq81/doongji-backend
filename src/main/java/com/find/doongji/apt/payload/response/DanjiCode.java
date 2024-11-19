4c6503d7c63fd0f9038d43c813f1f217dd6927ce
becooq81 <becooq81@gmail.com>
2024-11-19 09:59:32 +0900

    feat: add danji code to contain api response of search based on bjd code

--- src/main/java/com/find/doongji/apt/payload/response/DanjiCode.java
+++ src/main/java/com/find/doongji/apt/payload/response/DanjiCode.java
@@ -1,2 +1,14 @@
-package com.find.doongji.apt.payload.response;public class DanjiCode {
+package com.find.doongji.apt.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DanjiCode {
+    private String bjdCode;
+    private String kaptCode;
+    private String kaptName;
 }
