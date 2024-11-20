764d3f45bfb2d73d4c791bf27f31862e34a16ddb
becooq81 <becooq81@gmail.com>
2024-11-20 14:54:52 +0900

    feat: add auth user that extends userdetails, custom user details service that extends user details service

--- src/main/java/com/find/doongji/auth/payload/response/AuthUser.java
+++ src/main/java/com/find/doongji/auth/payload/response/AuthUser.java
@@ -1,2 +1,39 @@
-package com.find.doongji.auth.payload.response;public class AuthUser {
+package com.find.doongji.auth.payload.response;
+
+import com.find.doongji.member.payload.response.Member;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class AuthUser implements UserDetails {
+
+    private final Member member;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(member.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+
+    @Override
+    public String toString() {
+        return "AuthUser{" +
+                "member=" + member +
+                '}';
+    }
 }
--- src/main/java/com/find/doongji/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/find/doongji/auth/service/CustomUserDetailsService.java
@@ -1,2 +1,27 @@
-package com.find.doongji.auth.service;public class CustomUserDetailsService {
+package com.find.doongji.auth.service;
+
+import com.find.doongji.auth.payload.response.AuthUser;
+import com.find.doongji.member.payload.response.Member;
+import com.find.doongji.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username);
+        if (member == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        System.out.println("UserDetailsService: "+member.getUsername());
+        return new AuthUser(member);
+    }
 }
