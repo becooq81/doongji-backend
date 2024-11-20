8d0242bf696714094a808ab883cd8c0a7c9c0505
becooq81 <becooq81@gmail.com>
2024-11-21 06:35:41 +0900

    feat: add update request for listing, where id is necessary to identify which record to update

--- src/main/java/com/find/doongji/listing/payload/request/ListingUpdateRequest.java
+++ src/main/java/com/find/doongji/listing/payload/request/ListingUpdateRequest.java
@@ -1,4 +1,24 @@
 package com.find.doongji.listing.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ListingUpdateRequest {
+
+    @NotBlank
+    private Long id;
+
+    private MultipartFile image;
+    private String address;
+    private String aptDong;
+    private String description;
+
 }
