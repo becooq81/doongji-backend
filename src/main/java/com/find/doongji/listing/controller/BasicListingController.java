f93de16aebe83d11dc1c9dc5bcd08abb13f401cc
becooq81 <becooq81@gmail.com>
2024-11-21 06:34:55 +0900

    feat: set up listing controller

--- src/main/java/com/find/doongji/listing/controller/BasicListingController.java
+++ src/main/java/com/find/doongji/listing/controller/BasicListingController.java
@@ -1,19 +1,53 @@
 package com.find.doongji.listing.controller;
 
+import com.find.doongji.listing.payload.request.ListingCreateRequest;
+import com.find.doongji.listing.payload.request.ListingUpdateRequest;
 import com.find.doongji.listing.service.ListingService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/listing")
-public class ListingController {
+public class BasicListingController implements ListingController {
 
     private final ListingService listingService;
+
+    @Override
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_SELLER')")
+    public ResponseEntity<?> createListing(@RequestBody @Valid ListingCreateRequest request) {
+        return null;
+    }
+
+    @Override
+    @PutMapping
+    @PreAuthorize("hasRole('ROLE_SELLER')")
+    public ResponseEntity<?> updateListing(@RequestBody @Valid ListingUpdateRequest request) {
+        return null;
+    }
+
+    @Override
+    @DeleteMapping
+    @PreAuthorize("hasRole('ROLE_SELLER')")
+    public ResponseEntity<?> deleteListing(@RequestParam("id") Long id) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getListing(@RequestParam("id") Long id) {
+        return null;
+    }
+
+    @Override
+    @GetMapping("/{address}")
+    public ResponseEntity<?> getListings(@RequestParam("address") String address) {
+        return null;
+    }
+
 
 }
--- src/main/java/com/find/doongji/listing/controller/ListingController.java
+++ src/main/java/com/find/doongji/listing/controller/ListingController.java
@@ -1,4 +1,15 @@
 package com.find.doongji.listing.controller;
 
+import com.find.doongji.listing.payload.request.ListingCreateRequest;
+import com.find.doongji.listing.payload.request.ListingUpdateRequest;
+import org.springframework.http.ResponseEntity;
+
 public interface ListingController {
+
+    ResponseEntity<?> createListing(ListingCreateRequest request);
+    ResponseEntity<?> updateListing(ListingUpdateRequest request);
+    ResponseEntity<?> deleteListing(Long id);
+    ResponseEntity<?> getListing(Long id);
+    ResponseEntity<?> getListings(String address);
+
 }
