47f4ae95bb9b348b81d5577874c516cb784c0e82
becooq81 <becooq81@gmail.com>
2024-11-21 06:35:56 +0900

    feat: add dto to contain response from classification results

--- src/main/java/com/find/doongji/listing/payload/response/ClassificationResponse.java
+++ src/main/java/com/find/doongji/listing/payload/response/ClassificationResponse.java
@@ -1,4 +1,15 @@
 package com.find.doongji.listing.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClassificationResponse {
+
+    private int result;
 }
