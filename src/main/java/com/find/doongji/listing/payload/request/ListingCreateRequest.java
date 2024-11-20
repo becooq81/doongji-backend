d9b016ac5861376cfe6d2a84b1f4d2d31537ab60
becooq81 <becooq81@gmail.com>
2024-11-21 06:35:09 +0900

    feat: add dto to accept create requests

--- src/main/java/com/find/doongji/listing/payload/request/ListingCreateRequest.java
+++ src/main/java/com/find/doongji/listing/payload/request/ListingCreateRequest.java
@@ -1,4 +1,27 @@
 package com.find.doongji.listing.payload.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ListingCreateRequest {
+
+    @NotNull
+    private MultipartFile image;
+
+    // TODO: 프론트단에서 도로명주소인지 구주소인지 구분할 수 있나요?
+    @NotNull
+    private String address; // 도로명주소 또는 구주소 중 하나
+
+    private String aptDong;
+
+    private String description;
+
 }
