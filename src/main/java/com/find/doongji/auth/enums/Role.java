7735411a889ba6e7f8f14b06de12afa61569bc57
becooq81 <becooq81@gmail.com>
2024-11-20 14:54:22 +0900

    feat: add role for users to differentiate access to uploading listings

--- src/main/java/com/find/doongji/auth/enums/Role.java
+++ src/main/java/com/find/doongji/auth/enums/Role.java
@@ -1,2 +1,16 @@
-package com.find.doongji.auth.enums;public enum Role {
+package com.find.doongji.auth.enums;
+
+public enum Role {
+
+    ROLE_USER("ROLE_USER", "일반 사용자"),
+    ROLE_SELLER("ROLE_SELLER", "일반 판매자"),
+    ROLE_ADMIN("ROLE_ADMIN", "관리자");
+
+    private final String key;
+    private final String value;
+
+    Role(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
 }
