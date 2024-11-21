07aa36212703d449d6c0a495061da57f5418c43c
becooq81 <becooq81@gmail.com>
2024-11-22 03:02:19 +0900

    fix: replace return value

--- src/main/java/com/find/doongji/search/service/SearchService.java
+++ src/main/java/com/find/doongji/search/service/SearchService.java
@@ -2,13 +2,14 @@ package com.find.doongji.search.service;
 
 import com.find.doongji.apt.payload.response.AptInfo;
 import com.find.doongji.search.payload.request.SearchRequest;
+import com.find.doongji.search.payload.response.SearchResponse;
 import com.find.doongji.search.payload.response.SearchResult;
 
 import java.util.List;
 
 public interface SearchService {
 
-    List<AptInfo> search(SearchRequest searchRequest) throws Exception;
+    List<SearchResponse> search(SearchRequest searchRequest) throws Exception;
 
     SearchResult viewSearched(String aptSeq) throws Exception;
 }
