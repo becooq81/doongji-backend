e02b968005aa6b0a52141c5a0cc9b1f76bdee82b
becooq81 <becooq81@gmail.com>
2024-11-21 14:32:16 +0900

    feat: add address repository mapper

--- src/main/java/com/find/doongji/address/repository/AddressRepository.java
+++ src/main/java/com/find/doongji/address/repository/AddressRepository.java
@@ -1,2 +1,53 @@
-package com.find.doongji.address.repository;public interface AddressRepository {
+package com.find.doongji.address.repository;
+
+import com.find.doongji.address.payload.request.AddressMapping;
+import com.find.doongji.address.payload.response.AddressMappingResponse;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface AddressRepository {
+
+    /**
+     * @return 맵핑 테이블 존재 여부 반환
+     */
+    int checkIfMappingTableExists();
+
+    /**
+     * @return 맵핑 테이블 생성
+     */
+    void bulkInsertAddressMapping(@Param("list") List<AddressMapping> mappings);
+
+    /**
+     * @param danjiId
+     * @return danjiId에 해당하는 법정동 코드 반환
+     */
+    String selectBjdCodeByDanjiId(int danjiId);
+
+    /**
+     * @param addressMapping
+     * @return 맵핑 테이블에 주소 맵핑 추가 (맵핑 테이블에 존재하지 않는 주소만 추가)
+     */
+    Long insertAddressMapping(AddressMapping addressMapping);
+
+    /**
+     * @param roadNm
+     * @param roadNmBonbun
+     * @param roadNmBubun 도로명주소 숫자 부분에 다시가 없으면 0으로 대체
+     */
+    List<AddressMappingResponse> selectAddressMappingByDoroJuso(@Param("roadNm") String roadNm, @Param("roadNmBonbun") String roadNmBonbun, @Param("roadNmBubun") String roadNmBubun);
+
+    /**
+     * @param oldAddress 지번주소
+     * @return
+     */
+    List<AddressMappingResponse> selectAddressMappingByOldAddress(@Param("oldAddress") String oldAddress);
+
+    /**
+     * @param roadAddress 도로명주소
+     * @return
+     */
+    List<AddressMappingResponse> selectAddressMappingByRoadAddress(@Param("roadAddress") String roadAddress);
 }
