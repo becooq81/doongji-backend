5c778290d0d0c7d1418aa61b49b31d8e93f615eb
becooq81 <becooq81@gmail.com>
2024-11-21 14:36:10 +0900

    feat: add payload to contain retrieval of listing from table (includes id)

--- src/main/java/com/find/doongji/listing/payload/response/ListingResponse.java
+++ src/main/java/com/find/doongji/listing/payload/response/ListingResponse.java
@@ -1,4 +1,24 @@
 package com.find.doongji.listing.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@Builder
 public class ListingResponse {
+    private Long id;
+    private Long addressMappingId;
+    private String username;
+
+    private String imagePath;
+    private String oldAddress;
+    private String roadAddress;
+
+    private String aptDong;
+    private String aptHo;
+
+    private String description;
 }
