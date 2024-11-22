107d04f7d095255dd009baee39651db0136a9987
becooq81 <becooq81@gmail.com>
2024-11-22 16:19:08 +0900

    feat: add as1, as2, as3 fields

--- src/main/java/com/find/doongji/danji/payload/response/DanjiCode.java
+++ src/main/java/com/find/doongji/danji/payload/response/DanjiCode.java
@@ -14,14 +14,20 @@ public class DanjiCode {
     private String bjdCode;
     private String kaptCode;
     private String kaptName;
+    private String as1;
+    private String as2;
+    private String as3;
 
     @Override
     public String toString() {
         return "DanjiCode{" +
-                "roadAddress='" + siGugunDong + '\'' +
+                "siGugunDong='" + siGugunDong + '\'' +
                 ", bjdCode='" + bjdCode + '\'' +
                 ", kaptCode='" + kaptCode + '\'' +
                 ", kaptName='" + kaptName + '\'' +
+                ", as1='" + as1 + '\'' +
+                ", as2='" + as2 + '\'' +
+                ", as3='" + as3 + '\'' +
                 '}';
     }
 }
