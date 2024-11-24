a4814184efc504ea65dd75c0357a7e83fac7a9b2
becooq81 <becooq81@gmail.com>
2024-11-25 07:02:54 +0900

    feat: add dtos for review

--- src/main/java/com/find/doongji/review/payload/request/ReviewCreateRequest.java
+++ src/main/java/com/find/doongji/review/payload/request/ReviewCreateRequest.java
@@ -1,4 +1,15 @@
 package com.find.doongji.review.payload.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class ReviewCreateRequest {
+    private String aptSeq;
+    private String content;
 }
--- src/main/java/com/find/doongji/review/payload/response/ReviewSummaryResponse.java
+++ src/main/java/com/find/doongji/review/payload/response/ReviewSummaryResponse.java
@@ -1,4 +1,18 @@
 package com.find.doongji.review.payload.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ReviewSummaryResponse {
+
+    private String summary;
+    private List<ReviewResponse> reviews;
 }
