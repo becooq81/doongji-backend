c44799380add761e09ec52f31cb05b0935fb35e7
becooq81 <becooq81@gmail.com>
2024-11-25 07:02:35 +0900

    feat: add controller endpoint for creating reviews

--- src/main/java/com/find/doongji/review/controller/BasicReviewController.java
+++ src/main/java/com/find/doongji/review/controller/BasicReviewController.java
@@ -1,4 +1,28 @@
 package com.find.doongji.review.controller;
 
-public class BasicReviewController {
+import com.find.doongji.review.payload.request.ReviewCreateRequest;
+import com.find.doongji.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/review")
+public class BasicReviewController implements ReviewController{
+
+    private final ReviewService reviewService;
+
+    @Override
+    @PostMapping
+    public ResponseEntity<?> createReview(ReviewCreateRequest request) {
+        try {
+            reviewService.createReview(request);
+            return ResponseEntity.ok("Review created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
