f769675db8671a3b10306600c2c17878cca81c27
becooq81 <becooq81@gmail.com>
2024-11-25 07:03:06 +0900

    feat: implement repo for review

--- src/main/java/com/find/doongji/review/repository/ReviewRepository.java
+++ src/main/java/com/find/doongji/review/repository/ReviewRepository.java
@@ -1,4 +1,35 @@
 package com.find.doongji.review.repository;
 
+import com.find.doongji.review.payload.request.ReviewEntity;
+import com.find.doongji.review.payload.response.ReviewResponse;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
 public interface ReviewRepository {
+
+    /**
+     * @return review 테이블 존재 여부 확인
+     */
+    int checkIfTableExists();
+
+    /**
+     * review 레코드 한 번에 여러 개씩 추가
+     * @param reviewEntities
+     */
+    void bulkInsertReview(@Param("list") List<ReviewEntity> reviewEntities);
+
+    /**
+     * review 추가
+     * @param reviewEntity
+     */
+    void insertReview(ReviewEntity reviewEntity);
+
+    /**
+     * @param aptSeq 아파트 시퀀스
+     * @return 아파트 시퀀스에 해당하는 리뷰 조회
+     */
+    List<ReviewResponse> selectReviewsByAptSeq(@Param("aptSeq") String aptSeq);
 }
