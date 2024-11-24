d1386dd0b205724151c10a2675e8575a727d894c
becooq81 <becooq81@gmail.com>
2024-11-25 01:29:08 +0900

    chore: remove unused imports

--- src/main/java/com/find/doongji/like/controller/BasicLikeController.java
+++ src/main/java/com/find/doongji/like/controller/BasicLikeController.java
@@ -4,7 +4,6 @@ import com.find.doongji.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
