d943e7fdf3a2d6a13ab5dbef4d2d5e71feeb95d9
becooq81 <becooq81@gmail.com>
2024-11-25 01:23:50 +0900

    feat: add service methods for like

--- src/main/java/com/find/doongji/like/service/BasicLikeService.java
+++ src/main/java/com/find/doongji/like/service/BasicLikeService.java
@@ -1,4 +1,43 @@
 package com.find.doongji.like.service;
 
-public class BasicLikeService {
+import com.find.doongji.like.repository.LikeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.file.AccessDeniedException;
+
+@Service
+@RequiredArgsConstructor
+public class BasicLikeService implements LikeService {
+
+    private final LikeRepository likeRepository;
+
+    @Override
+    @Transactional
+    public void toggleLike(String aptSeq) throws AccessDeniedException {
+        if (!isLoggedIn()) {
+            throw new AccessDeniedException("You must be logged in to like.");
+        }
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        likeRepository.toggleLike(username, aptSeq);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public int isLiked(String aptSeq) {
+        if (!isLoggedIn()) {
+            return 0;
+        }
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return likeRepository.selectLike(username, aptSeq);
+    }
+
+    private boolean isLoggedIn() {
+        return SecurityContextHolder.getContext().getAuthentication() != null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+                && !"anonymousUser".equals(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    }
 }
--- src/main/java/com/find/doongji/like/service/LikeService.java
+++ src/main/java/com/find/doongji/like/service/LikeService.java
@@ -1,4 +1,18 @@
 package com.find.doongji.like.service;
 
+import java.nio.file.AccessDeniedException;
+
 public interface LikeService {
+
+    /**
+     * 좋아요 생성 (좋아요 존재 시 삭제)
+     * @param aptSeq
+     */
+    void toggleLike(String aptSeq) throws AccessDeniedException;
+
+    /**
+     * 좋아요 여부 조회
+     * @param aptSeq
+     */
+    int isLiked(String aptSeq);
 }
