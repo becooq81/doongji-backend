89f242be2c1d44a19824202d636ddabe862ce0bb
becooq81 <becooq81@gmail.com>
2024-11-25 14:58:33 +0900

    feat: add repository method to do pagination

--- src/main/java/com/find/doongji/search/repository/SearchRepository.java
+++ src/main/java/com/find/doongji/search/repository/SearchRepository.java
@@ -1,2 +1,13 @@
-package com.find.doongji.search.repository;public interface SearchRepository {
+package com.find.doongji.search.repository;
+
+import com.find.doongji.apt.payload.response.AptInfo;
+import com.find.doongji.search.payload.request.SearchQuery;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface SearchRepository {
+    List<AptInfo> filterBySearchQuery(@Param("searchQuery") SearchQuery searchQuery);
 }
